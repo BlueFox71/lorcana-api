234419dfa4e64a319cc275120d5ee409b4f50e37
BlueFox71 <jules.cannet@outlook.fr>
2024-06-12 23:31:15 +0200

    first commit after live

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-/**
- * const cards = require("../data/cards.json");
+const cards = require("../data/cards.json");
 
 const express = require("express");
 const app = express();
@@ -67,23 +66,4 @@ app.get("/api/cards", (req, res) => {
 
 app.listen(port, () => {
   console.info(`Server running at http://localhost:${port}`);
-});
-*/
-
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
 });
